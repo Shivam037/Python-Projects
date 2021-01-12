202ea7b009942e6ca0a359484466f998b17759e4
Shivam Singh <76594040+Shivam037@users.noreply.github.com>
2021-01-13 00:03:32 +0530

    Update notification.py

--- notification.py
+++ notification.py
@@ -4,7 +4,7 @@ while True:
     notification.notify(
         title = "Take a Break",
         message = "Sir Please take a Break .You are continuously working on PC for 45 minutes",
-        app_icon="C:\\data D\\Shivam\\pc.ico",
+        app_icon="path",
         timeout = 5
         )
     
